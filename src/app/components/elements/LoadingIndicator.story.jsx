d0e2e62e41a70681b05f5b3e246d8b0213f2a236
Iain Maitland <imaitland@gmail.com>
2017-11-21 10:02:15 -0500

    working storybook

---
+++ src/app/components/elements/LoadingIndicator.story.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import LoadingIndicator from './LoadingIndicator';
+import styles from './LoadingIndicator.scss';
+
+storiesOf('Condenser', module)
+    .add('LoadingIndicator', () => (
+        <LoadingIndicator />
+    ));
